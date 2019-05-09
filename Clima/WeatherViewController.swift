00af19ecc864432bd8d5dbdcce90e3e96893fe84
zafarivaev <45336941+zafarivaev@users.noreply.github.com>
2019-05-09 14:46:25 +0500

    Update WeatherViewController.swift

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -15,7 +15,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = ""
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
 
